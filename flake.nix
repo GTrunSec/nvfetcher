57126c099613438b080e6b90372b052e2818f23d
GTrunSec <gtrunsec@hardenedlinux.org>
2021-05-25 14:26:19 -0700

    fix warning and add devshell

--- flake.nix
+++ flake.nix
@@ -1,8 +1,11 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
-  outputs = { self, nixpkgs, flake-utils, flake-compat, ... }:
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
+    devshell-flake.url = "github:numtide/devshell";
+  };
+  outputs = { self, nixpkgs, flake-utils, flake-compat, devshell-flake, ... }:
     with flake-utils.lib;
     eachDefaultSystem
       (system:
@@ -11,24 +14,34 @@
             inherit system;
             overlays = [
               self.overlay
+              devshell-flake.overlay
             ];
           };
         in
         with pkgs; rec {
           defaultPackage = nvfetcher-bin;
-          devShell = with haskell.lib;
-            (addBuildTools (haskellPackages.nvfetcher) [
-              haskell-language-server
-              cabal-install
-              nvchecker
-              nix-prefetch-git
-            ]).envFunc { };
-          packages.nvfetcher-lib = with haskell.lib;
-            overrideCabal (haskellPackages.nvfetcher) (drv: {
-              haddockFlags = [
-                "--html-location='https://hackage.haskell.org/package/$pkg-$version/docs'"
+          devShell = with pkgs;
+            devshell.mkShell {
+              packages = [
+                cabal2nix
+                devEnv
               ];
-            });
+              commands = with pkgs; [
+                {
+                  name = "cabal2nvfetcher";
+                  command = "cd nix && cabal2nix ../. > default.nix";
+                  help = "call cabal2nix to nvfetcher";
+                }
+              ];
+            };
+          packages.nvfetcher-lib = with haskell.lib;
+            overrideCabal
+              (haskellPackages.nvfetcher)
+              (drv: {
+                haddockFlags = [
+                  "--html-location='https://hackage.haskell.org/package/$pkg-$version/docs'"
+                ];
+              });
           hydraJobs = {
             inherit packages;
           };
@@ -41,6 +54,7 @@
                 nvfetcher = prev.haskellPackages.callPackage ./nix { };
               };
             });
+
           nvfetcher-bin = with prev;
             with final.haskellPackages;
             lib.overrideDerivation
@@ -48,11 +62,20 @@
               (drv: {
                 nativeBuildInputs = drv.nativeBuildInputs ++ [ makeWrapper ];
                 postInstall = ''
-                  EXE=${lib.makeBinPath [ nvchecker nix-prefetch-git ]}
+                    EXE=${lib.makeBinPath [ nvchecker nix-prefetch-git ]}
                   wrapProgram $out/bin/nvfetcher \
-                    --prefix PATH : "$out/bin:$EXE"
+                  --prefix PATH : "$out/bin:$EXE"
                 '';
               });
+
+          devEnv = with prev; (haskell.lib.addBuildTools final.haskellPackages.nvfetcher
+            [
+              haskell-language-server
+              cabal-install
+              nvchecker
+              nix-prefetch-git
+            ]
+          );
         };
     };
 }
